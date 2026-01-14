78ed3af64851a0a9f250709a443e7365ec321278
k3nozsCs <tarakenzo3@gmail.com>
2026-01-14 08:57:57 +0700

    method tambah kali bagi

--- LatihanMethod1/Program.cs
+++ LatihanMethod1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void tambah()
+{
+    int a = 10, b = 2, c = 0;
+    c = a + b;
+    Console.WriteLine($"{a} + {b} = {c}");
+}
+
+//kurang
+static void kurang()
+{
+    int a = 10, b = 2, c = 0;
+    c = a - b;
+    Console.WriteLine($"{a} - {b} = {c}");
+}
+
+//kali
+static void kali()
+{
+    int a = 10, b = 2, c = 0;
+    c = a * b;
+    Console.WriteLine($"{a} * {b} = {c}");
+}
+
+//bagi
+static void bagi()
+{
+    int a = 10, b = 2, c = 0;
+    c = a / b;
+    Console.WriteLine($"{a} / {b} = {c}");
+}
+
+tambah();
+kurang();
+kali();
+bagi();
